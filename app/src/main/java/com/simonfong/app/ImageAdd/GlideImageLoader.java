99fb63ddb3f0647ce1a1dbd8dd6575e6bf0a2ebc
simonfong <fongzm_simon@163.com>
2018-11-09 14:06:40 +0800

    增加item拖拉修改位置

--- app/src/main/java/com/simonfong/app/ImageAdd/GlideImageLoader.java
+++ app/src/main/java/com/simonfong/app/ImageAdd/GlideImageLoader.java
@@ -7,33 +7,23 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.RequestOptions;
 import com.lzy.imagepicker.loader.ImageLoader;
-import com.simonfong.imageadd.R;
 
-
-/**
- * Created by liuwenzhuo on 2018/3/8.
- */
 
 public class GlideImageLoader implements ImageLoader {
 
     @Override
     public void displayImage(Activity activity, String path, ImageView imageView, int width,
-        int height) {
-        Glide.with(activity)
-            .applyDefaultRequestOptions(RequestOptions.placeholderOf(R.mipmap.ic_launcher)
-                .diskCacheStrategy(DiskCacheStrategy.ALL))
-            .load(path)
-            .into(imageView);
+                             int height) {
+
+        Glide.with(activity).applyDefaultRequestOptions(new RequestOptions().diskCacheStrategy(DiskCacheStrategy.ALL)).load
+                (path).into(imageView);
     }
 
     @Override
     public void displayImagePreview(Activity activity, String path, ImageView imageView, int width,
-        int height) {
-        Glide.with(activity)
-            .applyDefaultRequestOptions(RequestOptions.placeholderOf(R.mipmap.ic_launcher)
-                .diskCacheStrategy(DiskCacheStrategy.ALL))
-            .load(path)
-            .into(imageView);
+                                    int height) {
+        Glide.with(activity).applyDefaultRequestOptions(new RequestOptions().diskCacheStrategy(DiskCacheStrategy.ALL)).load
+                (path).into(imageView);
     }
 
     @Override
