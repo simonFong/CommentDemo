0a80849e7fe229128b561972eb3ad7632f747de6
simonfong <fongzm_simon@163.com>
2019-01-19 14:05:55 +0800

    拖动更换图片位置后刷新adapter数据源位置

--- app/src/main/java/com/simonfong/app/MainActivity.java
+++ app/src/main/java/com/simonfong/app/MainActivity.java
@@ -1,13 +1,39 @@
 package com.simonfong.app;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.simonfong.app.ImageAdd.EvaluateActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_evaluate)
+    Button mBtnEvaluate;
+    @BindView(R.id.btn_shop_car)
+    Button mBtnShopCar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.btn_evaluate, R.id.btn_shop_car})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btn_evaluate:
+                startActivity(new Intent(this, EvaluateActivity.class));
+                break;
+            case R.id.btn_shop_car:
+                break;
+        }
     }
 }
